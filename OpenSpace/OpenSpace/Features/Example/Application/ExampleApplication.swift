954e2def56eab95abad8e14c84f19eb0251e040b
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-24 19:35:12 +0700

    feat(example): call noop via dependency

--- OpenSpace/OpenSpace/Features/Example/Application/ExampleApplication.swift
+++ OpenSpace/OpenSpace/Features/Example/Application/ExampleApplication.swift
@@ -3,19 +3,28 @@ import ComposableArchitecture
 struct ExampleContainer: Reducer {
     struct State: Equatable {
         var actionCount = 0
-        var statusMessage = "Waiting for an example action."
+        var statusMessage = "Waiting for a NoOp response."
     }
 
     enum Action: Equatable {
         case primaryButtonTapped
+        case noOpResponse(String)
     }
+
+    @Dependency(\.noOp) private var noOp
 
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .primaryButtonTapped:
                 state.actionCount += 1
-                state.statusMessage = "Reducer handled \(state.actionCount) example action."
+                state.statusMessage = "Calling NoOp…"
+                return .run { send in
+                    await send(.noOpResponse(noOp.call()))
+                }
+
+            case let .noOpResponse(response):
+                state.statusMessage = response
                 return .none
             }
         }
