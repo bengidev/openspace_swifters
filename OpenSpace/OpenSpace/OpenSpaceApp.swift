81d2048c72c5ffbb9d6e6771ab9f2b4889efecbf
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-24 19:52:59 +0700

    Wire app composition root

--- OpenSpace/OpenSpace/OpenSpaceApp.swift
+++ OpenSpace/OpenSpace/OpenSpaceApp.swift
@@ -1,15 +1,35 @@
 import ComposableArchitecture
+import SwiftData
 import SwiftUI
 
 @main
 struct OpenSpaceApp: App {
+    private let modelContainer: ModelContainer
+    private let store: StoreOf<ExampleContainer>
+
+    init() {
+        let modelContainer = Self.makeModelContainer()
+
+        self.modelContainer = modelContainer
+        self.store = Store(initialState: ExampleContainer.State()) {
+            ExampleContainer()
+        } withDependencies: {
+            $0.noOp = .live
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ExampleView(
-                store: Store(initialState: ExampleContainer.State()) {
-                    ExampleContainer()
-                }
-            )
+            ExampleView(store: store)
+                .modelContainer(modelContainer)
+        }
+    }
+
+    private static func makeModelContainer() -> ModelContainer {
+        do {
+            return try ModelContainer(for: PersistedPlaceholder.self)
+        } catch {
+            fatalError("Failed to create SwiftData model container: \(error)")
         }
     }
 }
