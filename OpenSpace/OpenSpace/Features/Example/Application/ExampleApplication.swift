c33e35ec05dde0502a52bcb2bfe0acbbb3bb1c78
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-24 18:38:57 +0700

    feat(example): add TCA example container reducer

--- OpenSpace/OpenSpace/Features/Example/Application/ExampleApplication.swift
+++ OpenSpace/OpenSpace/Features/Example/Application/ExampleApplication.swift
@@ -1,3 +1,23 @@
-enum ExampleApplication {
-    static let summary = "Reducers, actions, state, and dependencies for the Example feature."
+import ComposableArchitecture
+
+struct ExampleContainer: Reducer {
+    struct State: Equatable {
+        var actionCount = 0
+        var statusMessage = "Waiting for an example action."
+    }
+
+    enum Action: Equatable {
+        case primaryButtonTapped
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .primaryButtonTapped:
+                state.actionCount += 1
+                state.statusMessage = "Reducer handled \(state.actionCount) example action."
+                return .none
+            }
+        }
+    }
 }
