d8b9897ce4a18920cfc9b3ac1da9ace8610fe2f7
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-24 17:40:08 +0700

    refactor(app): launch example shell

--- OpenSpace/OpenSpace/OpenSpaceApp.swift
+++ OpenSpace/OpenSpace/OpenSpaceApp.swift
@@ -1,32 +1,10 @@
-//
-//  OpenSpaceApp.swift
-//  OpenSpace
-//
-//  Created by Bambang Tri Rahmat Doni on 21/05/26.
-//
-
-import SwiftData
 import SwiftUI
 
 @main
 struct OpenSpaceApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExampleView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
